9df0edfb0dba329a8bc42b6fea8a9ee5e6b909eb
Sanaullahabbasi <136949915+Sanaullahabbasi@users.noreply.github.com>
2023-12-09 21:04:56 +0500

    working on Javascript to run analog clock

---
+++ app.js
@@ -0,0 +1,12 @@
+let hr = document.getElementById("hour");
+let mint = document.getElementById("min");
+let sec = document.getElementById("sec");
+
+displayTime = ()=>{
+let date = new Date();
+
+// getting hours, minutes and seconds
+
+}
+
+setInterval(displayTime, 1000)
